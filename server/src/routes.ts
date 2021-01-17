1533914cfb5557853f3b2affc0eb7b911e5dc722
r0cc4t0 <guilherme.roccato@outlook.com>
2021-01-17 15:50:48 -0300

    List of Items

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,20 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello, World!' });
+routes.get('/items', async (request, response) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map(item => {
+    return {
+      id: item.id,
+      title: item.title,
+      image: `http://localhost:3333/uploads/${item.image}`
+    };
+  });
+
+  return response.json(serializedItems);
 });
 
 export default routes;
