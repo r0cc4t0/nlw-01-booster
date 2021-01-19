0376b7f40c9784568f8dc0d8bc75910358fc9b7a
r0cc4t0 <guilherme.roccato@outlook.com>
2021-01-19 20:35:32 -0300

    Listing a Specific Collection Point

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -7,6 +7,8 @@ const itemsController = new ItemsController();
 const pointsController = new PointsController();
 
 routes.get('/items', itemsController.index);
+
+routes.get('/points/:id', pointsController.show);
 routes.post('/points', pointsController.create);
 
 export default routes;
