2d310bc390bd09c539519d859fef3191d83cc8fb
r0cc4t0 <guilherme.roccato@outlook.com>
2021-01-30 16:30:27 -0300

    Concepts: Elements and Styles

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,8 +5,8 @@ import { StatusBar } from 'expo-status-bar';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, World!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Hello, World!</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -14,8 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#7159c1',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  title: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold'
   }
 });
