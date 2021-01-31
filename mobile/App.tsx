d188d868a1c733e763f0549ad32585990706c82b
r0cc4t0 <guilherme.roccato@outlook.com>
2021-01-31 14:59:42 -0300

    Mobile Application Navigation Settings

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,7 +3,7 @@ import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'react-native';
 import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
 import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
-import Home from './src/pages/Home';
+import Routes from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -17,7 +17,7 @@ export default function App() {
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
-      <Home />
+      <Routes />
     </>
   );
 }
--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import { View, ImageBackground, Text, Image, StyleSheet } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import { Feather as Icon } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
+  const navigation = useNavigation();
+
+  function handleNavigateToPoints() {
+    navigation.navigate('Points');
+  }
+
   return (
     <ImageBackground
       source={require('../../assets/home-background.png')}
@@ -16,7 +23,7 @@ const Home = () => {
         <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
       </View>
 
-      <RectButton style={styles.button} onPress={() => { }}>
+      <RectButton style={styles.button} onPress={handleNavigateToPoints}>
         <View style={styles.buttonIcon}>
           <Icon name="arrow-right" color="#fff" size={24} />
         </View>
@@ -29,8 +36,7 @@ const Home = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 32,
-    backgroundColor: '#f0f0f5'
+    padding: 32
   },
 
   main: {
