7a12794a85632b2924b0b0771b37344cf7110799
r0cc4t0 <guilherme.roccato@outlook.com>
2021-01-31 13:04:29 -0300

    Mobile Application Homepage

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'react-native';
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
+import Home from './src/pages/Home';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular, Roboto_500Medium, Ubuntu_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello, World!</Text>
-      <StatusBar style="light" />
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+      <Home />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  title: {
-    color: '#fff',
-    fontSize: 20,
-    fontWeight: 'bold'
-  }
-});
